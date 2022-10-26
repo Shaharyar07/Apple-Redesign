ad6e19f71ebb725d8a341db479480f1105fa3cfa
Shaharyar07 <shaharyar.tariq248@gmail.com>
2022-10-27 00:12:13 +0500

    Header UI added 1:05

---
+++ components/Header.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+const Header = () => {
+  const session = false;
+  return (
+    <header className='sticky top-0 z-30 flex w-full  items-center justify-between bg-[#E7ECEE] p-4'>
+      <div className='flex items-center justify-center md:w-1/5'>
+        <Link href='/'>
+          <div className='relative h-10 w-5 cursor-pointer opacity-75 transition hover:opacity-100 '>
+            <Image
+              src='https://rb.gy/vsvv2o'
+              fill
+              className='object-contain'
+              alt='apple'
+            />
+          </div>
+        </Link>
+      </div>
+      <div className='hidden flex-1 items-center justify-center space-x-8 md:flex'>
+        <a className='headerLink'>Products</a>
+        <a className='headerLink'>Explore</a>
+        <a className='headerLink'>Support</a>
+        <a className='headerLink'>Business</a>
+      </div>
+      <div className='flex items-center justify-center space-x-4 md:w-1/5'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={1.5}
+          stroke='currentColor'
+          className='headerIcon'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z'
+          />
+        </svg>
+        <Link href='/checkout'>
+          <div className='relative cursor-pointer'>
+            <span className='absolute -right-1 z-50 -top-1 h-4 w-4 flex items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500'>
+              5
+            </span>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='headerIcon'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z'
+              />
+            </svg>
+          </div>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
