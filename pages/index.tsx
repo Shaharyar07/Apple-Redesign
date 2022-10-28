c0d5a090457c0da7b47ae9a29e072355a0274b15
Shaharyar07 <shaharyar.tariq248@gmail.com>
2022-10-28 18:29:34 +0500

    promo section initalized

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,9 +12,16 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Header />
-      <main>
+      <main className='relative h-[200vh] bg-[#E7ECEE]'>
         <Landing />
       </main>
+      <section className='relative z-40 -mt-[100vh] min-h-screen bg-[#1B1B1B]'>
+        <div className='space-y-10 py-16'>
+          <h1 className='text-center text-4xl font-medium tracking-wide text-white md:text-5xl'>
+            New Promo
+          </h1>
+        </div>
+      </section>
     </div>
   );
 };
